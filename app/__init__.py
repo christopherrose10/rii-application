385bc0c2abc3e007b95bbe2612bb8a70cd08b5a9
UA CR <blazer5@email.arizona.edu>
2022-01-14 10:29:47 -0700

    fetched api using requests library and viewable as a json

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,19 @@
 import requests
 
+# api that displays books
 books_api_url = 'https://p8doqtvi9f.execute-api.us-west-2.amazonaws.com/rii-dev-interview/books'
+
+# api that displays authors
 authors_api_url = 'https://p8doqtvi9f.execute-api.us-west-2.amazonaws.com/rii-dev-interview/authors'
 
+# requests endpoint with GET call 
 booksResponse = requests.get(books_api_url)
+# displays as JSON object
 books = booksResponse.json()
 
+# requests endpoint with GET call 
 authorResponse = requests.get(authors_api_url)
+# displays as JSON object
 authors = authorResponse.json()
 
 print(books)
