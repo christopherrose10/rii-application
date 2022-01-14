ba18eb79878537008a573b2258e88605fc47f7a6
UA CR <blazer5@email.arizona.edu>
2022-01-14 10:58:37 -0700

    created loop through each author that displays a list of each author and calculates when author was 10 years old

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,6 @@
 import requests
+from datetime import date, datetime
+from dateutil.relativedelta import relativedelta
 
 # api that displays books
 books_api_url = 'https://p8doqtvi9f.execute-api.us-west-2.amazonaws.com/rii-dev-interview/books'
@@ -16,6 +18,32 @@ authorResponse = requests.get(authors_api_url)
 # displays as JSON object
 authors = authorResponse.json()
 
+yearCurrent = datetime.today()
 
-print(books)
-print(authors)
+# list that contains authors and the book they could NOT have written
+listOfNotMyBooks = []
+
+# loop through each author 
+for i in authors:
+    # dict that will hold author
+    resultObj = {}
+
+    # stores authors name
+    authorName = str(i['name']) + '(could not have written these books)'
+    # stores authors age
+    age = int(i['age'])
+    # calculates and stores authors birth year by subtracting their age from current year
+    birthYear = yearCurrent - relativedelta(years=age)
+    # calculates and stores when author was 10
+    authorTenYrsOld = birthYear + relativedelta(years=10)
+
+    # pushes author's name as key to resultObj dict... empty list as value will be filled with books
+    resultObj[authorName] = []
+
+    print(authorTenYrsOld)
+
+    # pushes each dict containing author to 'listOfNotMyBooks' list
+    listOfNotMyBooks.append(resultObj)
+
+print(listOfNotMyBooks)
+
